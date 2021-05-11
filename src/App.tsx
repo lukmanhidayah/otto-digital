48466517e1ec11a4d4678bf8f1afe1153a4cea69
Lukman Hidayah <lukmanhidayah01@gmail.com>
2021-05-11 14:51:25 +0700

    Add login and registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import Container from "./components/container/Container";
+import { Route, Switch } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Registration from "./pages/registration/Registration";
 
 const App = () => {
-  return <Container />;
+  return (
+    <div className="app">
+      <div id="modal-root" />
+      <Switch>
+        <Route path={"/login"} exact={true} component={Login} />
+        <Route path={"/registration"} exact={true} component={Registration} />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/registration/Registration.tsx
@@ -0,0 +1,112 @@
+import { Link } from "react-router-dom";
+
+const Registration = () => {
+  return (
+    <div className="w-full min-h-screen bg-gray-50 flex items-center justify-center">
+      <div className="w-full py-4 md:w-6/12 lg:w-4/12 px-20 md:px-4">
+        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg border-0 bg-white">
+          <div className="rounded-t mb-0 px-6 py-6">
+            <div className="text-center mb-3">
+              <h1 className="text-blueGray-500 text-lg font-bold">Logo</h1>
+            </div>
+            <hr className="mt-6 border-b-1 border-blueGray-300" />
+          </div>
+          <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <form>
+              <div className="relative w-full mb-3">
+                <label
+                  className="block text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="company"
+                >
+                  Nama Perusahaan
+                </label>
+                <input
+                  type="text"
+                  id="company"
+                  className="border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-gray-200 focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="PT. Contoh Perusahaan"
+                />
+              </div>
+
+              <div className="relative w-full mb-3">
+                <label
+                  className="block text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Nama Pengguna
+                </label>
+                <input
+                  type="text"
+                  className="border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-gray-200 focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Nama Pengguna"
+                />
+              </div>
+
+              <div className="relative w-full mb-3">
+                <label
+                  className="block text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-gray-200 focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Email"
+                />
+              </div>
+
+              <div className="relative w-full mb-3">
+                <label
+                  className="block text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-gray-200 focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="relative w-full mb-3">
+                <label
+                  className="block text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Confirmation Password
+                </label>
+                <input
+                  type="password"
+                  className="border px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-gray-200 focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Confirmation Password"
+                />
+              </div>
+
+              <div className="text-center mt-6">
+                <button
+                  className="bg-blue-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                  type="button"
+                >
+                  Masuk
+                </button>
+              </div>
+            </form>
+            <div className="flex flex-wrap mt-6 relative">
+              <div className="w-full text-left">
+                <small>
+                  Sudah memiliki akun?{" "}
+                  <Link to="/login" className="text-blue-800">
+                    Masuk disini
+                  </Link>
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
