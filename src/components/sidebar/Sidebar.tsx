6552e15afad6765021f43ed5efffeecf7e354720
Lukman Hidayah <lukmanhidayah01@gmail.com>
2021-05-11 12:03:17 +0700

    Update sidebar ui

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,22 +1,37 @@
 import { Link } from "react-router-dom";
+
+import { ReactComponent as DashboardIcon } from "../../assets/svg/icons/dashboard.svg";
+import { ReactComponent as CategoryIcon } from "../../assets/svg/icons/category.svg";
+import { ReactComponent as ProductIcon } from "../../assets/svg/icons/product.svg";
+import { ReactComponent as TransactionIcon } from "../../assets/svg/icons/transaction.svg";
+
 import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <ul className="sidebar">
       <li>
-        <Link className="px-4 block p-2" to="/home">
-          Home
+        <Link className="sidebar-link active" to="/dashboard">
+          <DashboardIcon width={20} height={20} className="mr-4" />
+          Dashboard
         </Link>
       </li>
       <li>
-        <Link className="px-4 block p-2" to="/category">
-          category
+        <Link className="sidebar-link" to="/kategori">
+          <CategoryIcon width={20} height={20} className="mr-4" />
+          Kategori
         </Link>
       </li>
       <li>
-        <Link className="px-4 block p-2" to="/profile">
-          profile
+        <Link className="sidebar-link" to="/profile">
+          <ProductIcon width={20} height={20} className="mr-4" />
+          Produk
+        </Link>
+      </li>
+      <li>
+        <Link className="sidebar-link" to="/transaksi">
+          <TransactionIcon width={20} height={20} className="mr-4" />
+          Transaksi
         </Link>
       </li>
     </ul>
